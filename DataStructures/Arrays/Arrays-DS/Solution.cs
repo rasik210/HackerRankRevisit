4317e8a408b9a578711921d3ffde3fe68e44d6ee
Rasik Bihari <rasik210@gmail.com>
2026-08-11 12:29:04 +0530

    Arrays-DS problem

---
+++ DataStructures/Arrays/Arrays-DS/Solution.cs
@@ -0,0 +1,46 @@
+﻿
+using System.Collections.Generic;
+
+namespace HackerRankRevisit.DataStructures.Arrays.Arrays_DS
+{
+    /*
+         Problem: https://www.hackerrank.com/challenges/arrays-ds/problem
+         .NET Version: 8.0
+         IDE : Visual Studio Community 2022
+         Thoughts (Key points in algorithm):
+         - Traverse the input array from tail and store each element being traversed into a new array.
+         - Return the new array.
+
+
+         Gotchas:
+          <None>
+
+         Time Complexity:  O(n) //we need to traverse the entire array
+         Space Complexity: O(n) //we need an additional array having original array's size
+
+    */
+
+    internal class Solution
+    {
+        static void Main(string[] args)
+        {
+            var arrCount = Convert.ToInt32(Console.ReadLine().Trim());
+            var input = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+            var res = Result.ReverseArray(input);
+            Console.WriteLine(string.Join(" ", res));
+        }
+
+    }
+
+    internal class Result
+    {
+        public static List<int> ReverseArray(List<int> input)
+        {
+            var output = new List<int>();
+            for (int i = input.Count - 1; i >= 0; i--)
+                output.Add(input[i]);
+
+            return output;
+        }
+    }
+}   
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HackerRankRevisit
+{
+    internal class Program
+    {        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("There are multiple entry points in this project. You have landed at the default entry point. Every Solution.cs file in this project has its own entry point. If required, you can redirect the entry point to the desired solution file by setting the StartupObject attribute in HackerRankRevisit.csproj file.");
+        }
+    }
+}
